f8a072d9628e411ce129a3c58a6992e03f39f398
tzorca <tzorca@users.noreply.github.com>
2017-05-11 21:43:20 -0400

    Implement basic Tic-Tac-Toe moves

--- code/TicTacToeGame.js
+++ code/TicTacToeGame.js
@@ -1,45 +1,59 @@
-function TicTacToeGame(gridWidth, gridHeight) {
+var TicTacToeGame = function(gridWidth, gridHeight) {
 	var self = this;
 
 	var SYMBOLS = {
 		X: "X",
 		O: "O",
-		None: "_"
+		Empty: ""
 	};
 
 	var isPlayerXTurn = true;
 	var grid = [];
 
 	self.resetGame = function() {
-		for (var i = 0; i < gridHeight; i++) {
+		for (var vIdx = 0; vIdx < gridHeight; vIdx++) {
 			var gridRow = [];
-			for (var i = 0; i < gridWidth; i++) {
-				gridRow.push(SYMBOLS.Blank);
+			for (var hIdx = 0; hIdx < gridWidth; hIdx++) {
+				gridRow.push(SYMBOLS.Empty);
 			}
 			grid.push(gridRow);
 		}
 	};
+	self.resetGame();
 
 	self.getCurrentSymbol = function() {
 		return isPlayerXTurn ? SYMBOLS.X : SYMBOLS.O;
 	};
 
-	self.moveIsValid = function(symbol, col, row) {
-		if (col >= grid.length || col < 0) {
-			console.log("col out of bounds")
-			return false;
-		}
+	self.moveIsValid = function(symbol, row, col) {
+		console.log(col);
+		console.log(grid);
 
-		if (row >= grid[0].length || row < 0) {
+		if (row >= grid.length || row < 0) {
 			console.log("row out of bounds")
 			return false;
 		}
 
-		if (symbol != SYMBOLS.X && symbol != SYMBOLS.O)
+		if (col >= grid[0].length || col < 0) {
+			console.log("col out of bounds")
+			return false;
+		}
+
+		if (symbol != SYMBOLS.X && symbol != SYMBOLS.O) {
+			console.log("invalid symbol")
+			return false;
+		}
+
+		if (grid[row][col] != SYMBOLS.Empty) {
+			console.log("cell already occupied")
+			return false;
+		}
+
+		return true;
 	};
 
-	self.makeMove = function(col, row) {
-		if (!moveIsValid(self.getCurrentSymbol(), col, row) {
+	self.makeMove = function(row, col) {
+		if (!self.moveIsValid(self.getCurrentSymbol(), row, col)) {
 			alert("Invalid move.");
 			return;
 		}
@@ -47,7 +61,11 @@ function TicTacToeGame(gridWidth, gridHeight) {
 		grid[row][col] = self.getCurrentSymbol();
 
 		isPlayerXTurn = !isPlayerXTurn;
+		console.log("now turn " + self.getCurrentSymbol());
 	};
 
+	self.getGridState = function() {
+		return JSON.parse(JSON.stringify(grid));
+	};
 
 };
